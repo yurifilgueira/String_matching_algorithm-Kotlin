6373b28b91f4ea2f33e1042fc5a5d84067884165
Yuri FTM <yurimedeiros141@gmail.com>
2024-04-18 23:13:15 -0300

    Initial commit

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,26 +1,43 @@
 import util.DatasetReader
+import java.util.concurrent.ConcurrentHashMap
 
-fun main(args: Array<String>) {
-    val PATH = "resources\\train.csv"
+ fun main(args: Array<String>) {
+        val PATH = "resources\\train.csv"
 
-    val startTime = System.currentTimeMillis()
+        val startTime = System.currentTimeMillis()
 
-    val items: List<String> = ArrayList(
-        listOf(
-            "logitech",
-            "keyboard",
-            "mouse",
-            "hyperx",
-            "razer",
-            "lenovo",
-            "acer",
-            "lg",
-            "samsung",
-            "laptop"
+        val items: List<String> = ArrayList(
+            listOf(
+                "logitech",
+                "keyboard",
+                "mouse",
+                "hyperx",
+                "razer",
+                "lenovo",
+                "acer",
+                "lg",
+                "samsung",
+                "laptop"
+            )
         )
-    )
 
-    DatasetReader.readDatasetAndProcess(PATH, items)
+        val threads: MutableList<Thread> = ArrayList()
 
-    println("Total read and print time: " + (System.currentTimeMillis() - startTime).toDouble() / 60000)
-}
+        val matches: ConcurrentHashMap<String, Int> = ConcurrentHashMap<String, Int>()
+
+        var i = 0
+        while (i < items.size) {
+            val t = Thread(DatasetReader(items[i], items[++i], matches))
+            t.start()
+            threads.add(t)
+            i++
+        }
+
+        for (thread in threads) {
+            thread.join()
+        }
+
+        println("Total read and print time: " + (System.currentTimeMillis() - startTime).toDouble() / 60000)
+        // System.out.println("Count: " + count);
+        matches.forEach { (k: Any, v: Any) -> println("Match: $k - $v") }
+ }
--- src/main/kotlin/util/DatasetReader.kt
+++ src/main/kotlin/util/DatasetReader.kt
@@ -1,60 +1,81 @@
 package util
 
+import util.ResultSaver.save
 import java.io.IOException
 import java.nio.file.Files
 import java.nio.file.Paths
-import java.util.*
+import java.util.concurrent.ConcurrentHashMap
 
-class DatasetReader {
-    private var PATH: String? = null
+class DatasetReader : Runnable {
+    private var item: String? = null
+    private var item2: String? = null
+    private var matches: ConcurrentHashMap<*, *>? = null
 
     constructor()
 
-    constructor(PATH: String?) {
-        this.PATH = PATH
+    constructor(item: String?, matches: ConcurrentHashMap<*, *>?) {
+        this.item = item
+        this.matches = matches
     }
 
-    companion object {
-        private val lines: List<String>? = null
+    constructor(item: String?, item2: String?, matches: ConcurrentHashMap<*, *>?) {
+        this.item = item
+        this.item2 = item2
+        this.matches = matches
+    }
 
-        fun readDatasetAndProcess(PATH: String?, items: List<String>) {
-            val matches: MutableMap<String, Int> = HashMap()
+    fun readDatasetAndProcess() {
 
-            try {
-                Files.newBufferedReader(Paths.get(PATH)).use { br ->
-                    var counter = 0
-                    var line: String?
-                    while ((br.readLine().also { line = it }) != null) {
-                        counter++
-                        val arrayRatingLine =
-                            line?.split(",".toRegex())?.dropLastWhile { it.isEmpty() }
-                                ?.toTypedArray()
-                        val rating =
-                            arrayRatingLine?.get(2)?.replace("\"".toRegex(), "")?.lowercase(Locale.getDefault())
-                        val words =
-                            rating?.split(" ".toRegex())?.dropLastWhile { it.isEmpty() }
-                                ?.toTypedArray()
+        var count = 0
+        var count1 = 0
+        try {
+            Files.newBufferedReader(Paths.get(PATH)).use { br ->
+                var counter = 0
+                var line: String?
+                Thread.sleep(1000)
+                println(Thread.currentThread().name + " começou.")
+                while ((br.readLine().also { line = it }) != null) {
+                    counter++
+                    val arrayRatingLine =
+                        line?.split(",".toRegex())?.dropLastWhile { it.isEmpty() }
+                            ?.toTypedArray()
+                    val rating =
+                        arrayRatingLine?.get(2)?.replace("\"".toRegex(), "")?.toLowerCase()
+                    val words =
+                        rating?.split(" ".toRegex())?.dropLastWhile { it.isEmpty() }?.toTypedArray()
 
-                        for (item in items) {
-                            if (words != null) {
-                                for (word in words) {
-                                    if (LevenshteinDistance.calculateDistance(item, word) == 0) {
-                                        matches[word] = matches.getOrDefault(word, 0) + 1
-                                        println("Match: $item - $word")
-                                        break
-                                    }
-                                }
+                    if (words != null) {
+                        for (word in words) {
+                            if (LevenshteinDistance.calculateDistance(item!!, word) === 0) {
+                                count++
+                                println("Match: $item - $word")
+                                break
+                            } else if (LevenshteinDistance.calculateDistance(item2!!, word) === 0) {
+                                count1++
+                                println("Match: $item2 - $word")
+                                break
                             }
                         }
                     }
-
-                    println("Count: $counter")
-                    matches.forEach { (k: String, v: Int) -> println("Match: $k - $v") }
-                    ResultSaver.save(matches)
                 }
-            } catch (e: IOException) {
-                throw RuntimeException(e)
+
+                println("Count: $counter")
+                save(count, count1)
             }
+        } catch (e: IOException) {
+            throw RuntimeException(e)
+        } catch (e: InterruptedException) {
+            throw RuntimeException(e)
         }
+    }
+
+    override fun run() {
+        val startTime = System.currentTimeMillis()
+        readDatasetAndProcess()
+        println("Total read and print time of " + Thread.currentThread().name + ": " + (System.currentTimeMillis() - startTime).toDouble() / 60000)
+    }
+
+    companion object {
+        private const val PATH = "resources\\train.csv"
     }
 }
